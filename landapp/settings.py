cdc1cc1a8f6e728aa14be16c5a7b0cc962fa13aa
Jin <david.jeong0611@gmail.com>
2021-01-07 14:23:11 +0900

    remove useless setting for picking up dynamic ELB health check

--- landapp/settings.py
+++ landapp/settings.py
@@ -46,40 +46,6 @@ DEBUG = True
 # See https://docs.djangoproject.com/en/2.1/ref/settings/
 ALLOWED_HOSTS = ['landapp-dev-env.eba-hbggz7bm.ap-northeast-2.elasticbeanstalk.com']
 
-# Dynamically add EC2 IP address to ALLOWED_HOSTS
-# https://stackoverflow.com/a/54739284
-# https://hashedin.com/2017/01/06/5-gotchas-with-elastic-beanstalk-and-django/
-# def is_ec2_linux():
-#     """Detect if we are running on an EC2 Linux Instance
-#        See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
-#     """
-#     if os.path.isfile("/sys/hypervisor/uuid"):
-#         with open("/sys/hypervisor/uuid") as f:
-#             uuid = f.read()
-#             return uuid.startswith("ec2")
-#     return False
-#
-#
-# def get_linux_ec2_private_ip():
-#     """Get the private IP Address of the machine if running on an EC2 linux server"""
-#     from urllib.request import urlopen
-#     if not is_ec2_linux():
-#         return None
-#     try:
-#         response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
-#         ec2_ip = response.read().decode('utf-8')
-#         if response:
-#             response.close()
-#         return ec2_ip
-#     except Exception as e:
-#         print(e)
-#         return None
-
-#
-# private_ip = get_linux_ec2_private_ip()
-# if private_ip:
-#     ALLOWED_HOSTS.append(private_ip)
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
