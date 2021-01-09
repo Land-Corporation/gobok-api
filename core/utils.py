fc759e963453a81ba7285fcf0507ce457a82b711
Jin <david.jeong0611@gmail.com>
2021-01-09 15:32:43 +0900

    make thumbnail sqaure

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,6 @@
 import io
 
-from PIL import Image
+from PIL import Image, ImageOps
 from django.conf import settings
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
@@ -15,7 +15,7 @@ def process_image_data_from_request(request) -> bytes:
 
 def convert_image_to_thumbnail(original_image: bytes) -> bytes:
     image = Image.open(io.BytesIO(original_image))
-    image.thumbnail(settings.THUMBNAIL_DIMENSION, Image.ANTIALIAS)
+    image = ImageOps.fit(image, settings.THUMBNAIL_DIMENSION, Image.ANTIALIAS)
     buf = io.BytesIO()
     image.save(buf, format='JPEG', quality=100)
     return buf.getvalue()
